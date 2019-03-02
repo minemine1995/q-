ee8269e0f49f37e52c924b5f34b122a574a33d9b
chenyanbo <792512433@qq.com>
2019-03-02 13:48:00 +0800

    8个节点的最短路径，包括动态情形，使用模拟退火算法

--- ql.m
+++ ql.m
@@ -1,33 +1,33 @@
 clear
 clc
-states=6;       %初始数量
-actions=6;      %动作数量
-final_state=6; %目标状态
-episode=40;    %迭代次数
+states=8;       %初始数量
+actions=8;      %动作数量
+final_state=8; %目标状态
+episode=100;    %迭代次数
 gamma=1;        %折扣系数
 alpha=0.8;      %学习速率
 p_max=zeros(states,actions);%按Q值表选最大值进行动作的概率
-T=1;              %退火温度
-reward=[
-%  1    2    3    4    5     6 
- -inf -inf -inf -6   -8    -inf; %1
- -inf -inf -9   -5   -inf  -7  ; %2
- -inf -9   -inf -3   -inf  -inf; %3
- -6   -5   -3   -inf  -7   -inf; %4
- -8   -inf -inf -7   -inf  -5  ; %5
- -inf -10  -inf -inf -10   -inf     %6
- ];
+T=5;              %退火温度
+reward=-1*[inf	inf	47	inf	inf	24	inf	inf;
+inf	inf	55	31	31	inf	74	79;
+47	55	inf	88	23	25	66	inf;
+inf	31	88	inf	inf	120	inf	29;
+inf	31	23	inf	inf	inf	42	inf;
+24	inf	25	120	inf	inf	inf	inf;
+inf	74	66	inf	42	inf	inf	66;
+inf	79	inf	29	inf	inf	66	inf];
+tic
 Q_table=zeros(states,states);
 for i=1:episode
     current_state=randperm(states,1);                           %随机指定一个起始节点
     while current_state~=final_state;                           %当前节点是否为目标节点
-        optinal_action=find(reward(current_state,:)>=-10);      %当前状态的可能动作集
+        optinal_action=find(reward(current_state,:)>=-1000);      %当前状态的可能动作集
         if length(optinal_action)==0                            %判断当前状态的可能动作集是否为空
            print("not connected")
            break
         else
             next_action=choose_next_action(Q_table,current_state,optinal_action,p_max);     %根据退火算法选择当前状态的一个动作
-            next_state_optinal=find(reward(next_action,:)>=-10);                                    
+            next_state_optinal=find(reward(next_action,:)>=-1000);                                    
             maxQ=max(Q_table(next_action,next_state_optinal));
             Q_current=Q_table(current_state,next_action);
             Q_table(current_state,next_action)=(1-alpha)*Q_table(current_state,next_action)+alpha*(reward(current_state,next_action)+gamma*maxQ);
@@ -38,19 +38,20 @@ for i=1:episode
     
     end
     if i==episode/2
-                reward=[
-                %  1    2    3    4    5     6 
-                 -inf -inf -inf -6   -8    -inf; %1
-                 -inf -inf -9   -5   -inf  -7  ; %2
-                 -inf -9   -inf -3   -inf  -inf; %3
-                 -6   -5   -3   -inf  -7   -8; %4
-                 -8   -inf -inf -7   -inf  -5  ; %5
-                 -inf -10  -inf -8 -10   -inf     %6
-                 ];
+                reward=-1*[inf	inf	47	inf	inf	24	inf	inf;
+inf	inf	55	31	31	inf	74	79;
+47	55	inf	88	23	25	66	inf;
+inf	31	88	inf	inf	120	inf	59;
+inf	31	23	inf	inf	inf	42	inf;
+24	inf	25	120	inf	inf	inf	inf;
+inf	74	66	inf	42	inf	inf	66;
+inf	79	inf	59	inf	inf	66	inf];
+
              Q_table
-            end
+    end
 end
 Q_table
+toc
 
 function [next_action]=choose_next_action(Q_table,current_state,optinal_action,p_max)
     [max_Q,max_action]=max(Q_table(current_state,optinal_action));
